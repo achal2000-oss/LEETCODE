d49b8fca958b61c4afd52b6bb12e9f934000ef17
achal2000-oss <58485865+achal2000-oss@users.noreply.github.com>
2022-07-03 10:34:28 +0530

    Time: 62 ms (57.45%), Space: 16 MB (75.21%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
@@ -9,23 +9,40 @@ public:
     int maximumUnits(vector<vector<int>>& box, int Size)
     {
           
-        sort(box.begin(),box.end(),cmp);
-        //priority_queue<vector<int>,vector<vector<int>>,cmp1>pq
-        int sum=0;
-        for(int i=0;i<box.size();i++)
+        // sort(box.begin(),box.end(),cmp);
+        // //priority_queue<vector<int>,vector<vector<int>>,cmp1>pq
+        // int sum=0;
+        // for(int i=0;i<box.size();i++)
+        // {
+        //     if(Size>=box[i][0])
+        //     {
+        //         sum+=(box[i][1]*box[i][0]);
+        //         Size-=box[i][0];
+        //     }
+        //     else
+        //     {
+        //        sum+=(Size*box[i][1]); 
+        //         break;
+        //     }
+        // }
+        int maxi=INT_MIN,sum=0;
+        int bucket[1005]={0};
+        for(auto &a:box)
         {
-            if(Size>=box[i][0])
-            {
-                sum+=(box[i][1]*box[i][0]);
-                Size-=box[i][0];
-            }
-            else
-            {
-               sum+=(Size*box[i][1]); 
-                break;
-            }
+            maxi=max(a[1],maxi);
+            bucket[a[1]]+=a[0];
         }
-        
+        for(int i=maxi;i>=0;i--)
+        {
+            if(bucket[i]==0)
+                continue;
+            int n=bucket[i];
+            int c=min(n,Size);
+            sum+=(c*i);
+            Size-=c;
+            if(!Size)
+                break;
+        }
         return sum;
         //priority_queue<int,vector<int>,help>pq;
     }
