3b3fb446ba5eda3a99b6753ca7931c19348aa7ea
achal2000-oss <58485865+achal2000-oss@users.noreply.github.com>
2022-08-08 10:39:03 +0530

    Time: 11 ms (91.38%), Space: 10.5 MB (47.88%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -4,27 +4,44 @@ public:
     {
         int l=nums.size();
         
-        vector<int>dp(l,1);
-        dp[0]=1;
-        for(int i=0;i<l;i++)
-        {
-            for(int j=i+1;j<l;j++)
-            {
-                if(nums[i]<nums[j])
-                    dp[j]=max(dp[j],(dp[i]+1));
-            }
-            // for(int k=0;k<l;k++)
-            // cout<<dp[k]<<" ";
-            // cout<<endl;
-        }
+        vector<int>dp(l+1,INT_MAX);
+//         dp[0]=1;
+//         for(int i=0;i<l;i++)
+//         {
+//             for(int j=i+1;j<l;j++)
+//             {
+//                 if(nums[i]<nums[j])
+//                     dp[j]=max(dp[j],(dp[i]+1));
+//             }
+//             // for(int k=0;k<l;k++)
+//             // cout<<dp[k]<<" ";
+//             // cout<<endl;
+//         }
         
+//         int maxi=0;
+//         for(int i=0;i<l;i++)
+//         {
+//             if(maxi<dp[i])
+//                 maxi=dp[i];
+//         }
+        
+        dp[0]=nums[0];
+        for(int i=1;i<l;i++)
+        {
+            auto it=upper_bound(dp.begin(),dp.end(),nums[i]);
+            //cout<<*it<<" ";
+            if(it!=dp.begin() && dp[it-dp.begin()-1]==nums[i])
+                continue;
+            else
+                dp[it-dp.begin()]=nums[i];
+                
+        }
         int maxi=0;
-        for(int i=0;i<l;i++)
+        for(int i=l-1;i>=0;i--)
         {
-            if(maxi<dp[i])
-                maxi=dp[i];
+            if(dp[i]!=INT_MAX)
+                return i+1;
         }
-        
-        return maxi;
+        return -1;
     }
 };
