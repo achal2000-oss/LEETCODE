fd151a7e94010eb21a6f9828087a88d687e635c2
achal2000-oss <58485865+achal2000-oss@users.noreply.github.com>
2024-08-20 10:39:29 +0530

    Time: 370 ms (73.8%), Space: 70.1 MB (28.23%) - LeetHub

--- 1258-article-views-i/1258-article-views-i.py
+++ 1258-article-views-i/1258-article-views-i.py
@@ -1,5 +1,9 @@
 import pandas as pd
 
 def article_views(views: pd.DataFrame) -> pd.DataFrame:
-    return pd.DataFrame(views.query("author_id == viewer_id")['author_id'].unique(),columns = ['id']).sort_values(['id'])
+    return (((views.query("author_id == viewer_id")[['author_id']]
+                ).sort_values(['author_id'],ascending = [True])
+                ).drop_duplicates(subset = ['author_id'], keep = 'first')
+                ).rename(columns = {'author_id': "id"})
+
     
